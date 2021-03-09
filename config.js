27e0800c6d9a9d9361cf5fc33dcae13a72dabf64
syakhisk <mail.syakhisk@gmail.com>
2021-03-09 13:44:59 +0700

    kelebihan /

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const dev = process.env.NODE_ENV !== "production";
 const siteurl_prod = "https://joki-presensi.sakis.me";
 const siteurl_dev = "http://localhost:3000";
 
-const serverurl_prod = "https://jokipresensi-server.herokuapp.com/";
+const serverurl_prod = "https://jokipresensi-server.herokuapp.com";
 const serverurl_dev = "http://localhost:3001";
 
 module.exports = {
